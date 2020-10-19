8cddaeba226b194e060efdaafba328aefab1a067
Renzo Salinas <renzos.f15@gmail.com>
2020-10-19 07:44:05 -0500

    Ordenando y agregando claselab

---
+++ calculadoramvc/Controllers/ContactoController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using calculadoramvc.Models;
+using calculadoramvc.Data;
+
+namespace calculadoramvc.Controllers
+{
+    public class ContactoController : Controller
+    {
+
+       private readonly ILogger<ContactoController> _logger;
+       private readonly DatabaseContext _context;
+
+
+        public ContactoController(ILogger<ContactoController> logger,
+            DatabaseContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(Contacto objContacto){
+            if (ModelState.IsValid)
+            {
+                _context.Add(objContacto);
+                _context.SaveChanges();
+                objContacto.Response = "Gracias estamos en contacto";
+            }
+            return View("index", objContacto);
+        }
+
+    }
+}
